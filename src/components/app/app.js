01628e45824cab72db6fd4c733d82108ef510deb
Anastasia <nastyaerm@mail.ru>
2021-12-24 19:54:23 +0300

    create app, add props

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import './app.css';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+function App() {
+
+    const data = [
+        {name: "John G.", salary: 1800, increase: true},
+        {name: "Mike D.", salary: 3000, increase: false},
+        {name: "Jane X.", salary: 5500, increase: false},
+    ];
+
+
+
+    return (
+        <div className="app">
+            <AppInfo/>
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data} />
+
+            <EmployeesAddForm/>
+        </div>
+    );
+}
+
+export default App;
